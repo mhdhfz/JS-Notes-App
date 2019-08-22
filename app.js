0fd46eeed78c4b149b25cf320f5e4dd35b48afff
Hafiz Mustafa <mhd.hfzz@gmail.com>
2019-08-22 16:33:34 +0800

    Add files via upload
    import on ui
    add more logic

--- app.js
+++ app.js
@@ -1,10 +1,45 @@
-const edit = document.querySelector("#content");
+const title = document.querySelector("#title");
+const content = document.querySelector("#content");
+const listNotes = document.querySelector(".list-notes");
+let getTitle = document.querySelector("#title");
 let getContent = document.getElementById("content");
 
-getContent.innerHTML = localStorage.getItem("title");
-//getContent = localStorage.getItem("text");
+const dtlNote = localStorage.getItem("title");
+
+const generateTemplate = dtlNote => {
+  const html = `
+    <li class="list-note">
+      <span>${dtlNote}</span>
+       <i class="far fa-trash-alt delete"></i>
+    </li>
+  `;
+
+  listNotes.innerHTML += html;
+};
+
+getTitle.innerHTML = localStorage.getItem("title");
+getContent.innerHTML = localStorage.getItem("content");
 
 setInterval(() => {
-  localStorage.setItem("title", edit.innerHTML);
-  //localStorage.setItem("text", edit.innerHTML);
+  localStorage.setItem("content", content.innerHTML);
 }, 1000);
+
+title.addEventListener("keydown", e => {
+  if (e.keyCode == 9) {
+    localStorage.setItem("title", title.innerHTML);
+    generateTemplate(dtlNote);
+  }
+});
+
+title.addEventListener("keydown", e => {
+  if (
+    e.keyIdentifier === "U+000A" ||
+    e.keyIdentifier === "Enter" ||
+    e.keyCode === 13
+  ) {
+    if (e.target.nodeName == "DIV" && e.target.id == "title") {
+      e.preventDefault();
+      return false;
+    }
+  }
+});
